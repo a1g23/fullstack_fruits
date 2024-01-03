2ce6775a20b8ca058c35f697c051a2c77a432453
Alex Greenberg <acgreenberg2@gmail.com>
2024-01-02 19:27:46 -0500

    added the schema and model for Fruits

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ mongoose.connection.on("open", () => console.log("connected to mongoDB"))
 mongoose.connection.on("close", () => console.log("disconnected to mongoDB"))
 mongoose.connection.on("error", (err) => console.log(err))
 
+
+// create our Fruits Model
+
+//destructure Schema and model into their own models
+const {Schema, model} = mongoose
+//same thing as: 
+//const Schema = mongoose.Schema
+//const model = mongoose.model
+
+//make the Schema for fruit or SHAPE of the data
+const fruitSchema = new Schema ({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+// attach the Schema to a model (the object for interacting with the database) - singular and uppercase first
+const Fruit = model("Fruit", fruitSchema)
+
+
 // run the app
 const app = express()
 

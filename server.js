1b763bdfae5f753adfb808e5a88e1db7a3cb61a8
Alex Greenberg <acgreenberg2@gmail.com>
2024-01-02 19:18:05 -0500

    setup connection

--- server.js
+++ server.js
@@ -4,9 +4,24 @@
 require("dotenv").config() // Load ENV Variables
 const express = require("express") // import express
 const morgan = require("morgan") //import morgan
-const methodOverride = require("method-override")
-const mongoose = require("mongoose")
+const methodOverride = require("method-override") // override for forms
+const mongoose = require("mongoose") // connect to our mongodb
 
+// Database Collection
+const DATABASE_URL = process.env.DATABASE_URL
+const PORT = process.env.PORT
+
+//establish our connection
+
+mongoose.connect(DATABASE_URL)
+
+// events to help you understand when connection changes
+
+mongoose.connection.on("open", () => console.log("connected to mongoDB"))
+mongoose.connection.on("close", () => console.log("disconnected to mongoDB"))
+mongoose.connection.on("error", (err) => console.log(err))
+
+// run the app
 const app = express()
 
 
